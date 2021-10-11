dae251f280089ef25ee0f9ed08a96483e6cc534f
Pawel Kaminski <kaminski.pawel404@gmail.com>
2021-10-11 11:21:10 +0200

    initial commit

---
+++ Draughts/Draughts/Board.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Draughts
+{
+    class Board
+    {
+        public Pawn[,] Fields { get; set; }
+
+        public Board(int n)
+        {
+
+        }
+    }
+}
---
+++ Draughts/Draughts/Pawn.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Draughts
+{
+    class Pawn
+    {
+        public string Color { get; set; }
+
+        public Pawn(string color)
+        {
+            Color = color;
+        }
+    }
+}
